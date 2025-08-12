c65446ad39b6f4d0b2bb5d3173d1f02628fb29d0
Andrii <andreyoshlakov18051986@gmail.com>
2025-08-12 19:40:34 +0300

    some changes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,71 +1,41 @@
-import Section from "../Section/Section";
-import Container from "../Container/Container";
-import Form from "../Form/Form";
-import { useState } from "react";
-import { Photo } from "../../types/photo";
-import { getPhotos } from "../../services/photos";
-import toast from "react-hot-toast";
-import Loader from "../Loader/Loader";
-import Text from "../Text/Text";
-import PhotosGallery from "../PhotosGallery/PhotosGallery";
-import Modal from "../Modal/Modal";
+// 1. Імпортуємо хук useMutation
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+
+interface newTodo {
+  title: string;
+  completed: boolean;
+}
 
 export default function App() {
-  const [photos, setPhotos] = useState<Photo[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
+  // 2. Використовуємо хук
+  const mutation = useMutation({
+    mutationFn: async (newTodo: newTodo) => {
+      const res = await axios.post(
+        "https://jsonplaceholder.typicode.com/todos",
+        newTodo
+      );
+      return res.data;
+    },
+    onSuccess: () => {
+      console.log("Todo added successfully");
+    },
+  });
 
-  const openModal = () => setIsModalOpen(true);
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedPhoto(null);
+  const handleCreateTodo = () => {
+    // 3. Викликаємо mutate для того щоб виконати HTTP-запит
+    mutation.mutate({
+      title: "My new todo",
+      completed: false,
+    });
   };
 
-  const handleSearch = async (query: string) => {
-    try {
-      setPhotos([]);
-      setIsLoading(true);
-      setIsError(false);
-      const data = await getPhotos(query);
-      console.log(data);
-      if (data.length === 0) {
-        toast.error("No movies found for your request.", {
-          duration: 4000,
-          position: "top-center",
-          removeDelay: 1000,
-        });
-      }
-
-      setPhotos(data);
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const handlePhotoSelect = (photo: Photo) => {
-    setSelectedPhoto(photo);
-    openModal();
-  };
   return (
     <>
-      <Section>
-        <Container>
-          <Form onSubmit={handleSearch} />
-          {isLoading && <Loader />}
-          {isError && <Text>BIG ERROR!!!</Text>}
-          {photos.length > 0 && (
-            <PhotosGallery photos={photos} onSelect={handlePhotoSelect} />
-          )}
-          {isModalOpen && selectedPhoto !== null && (
-            <Modal onClose={closeModal} photo={selectedPhoto} />
-          )}
-        </Container>
-      </Section>
+      <button onClick={handleCreateTodo}>Create Todo</button>
+      {mutation.isPending && <div>Adding todo...</div>}
+      {mutation.isError && <div>An error occurred</div>}
+      {mutation.isSuccess && <div>Todo added!</div>}
     </>
   );
 }
